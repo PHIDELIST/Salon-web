ba7acc4ba9089bf3b62a8fce242f3859d506ff52
PHIDELIST <phidelisoluoch@gmail.com>
2023-02-22 15:44:22 +0300

    salon to be dockerized

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+#<?php
+   $con=mysqli_connect('salon-management.ce9a8miftq7p.us-east-1.rds.amazonaws.com','root','Manguro#!12','salon_management');
+?>#
+
+<?php
+//all the variables defined here are accessible in all the files that include this one
+#$con= new mysqli('localhost','root','','salon_managment(1)')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
